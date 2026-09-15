7f929f24905472b9f22887beac6cb8de4af407e0
Saif Kalkeri <kalkerisaif@gmail.com>
2026-09-16 03:12:09 +0530

    feat: redesign work page with detailed case studies and technical architecture

--- src/app/pages/work/case-studies/case-studies.module.ts
+++ src/app/pages/work/case-studies/case-studies.module.ts
@@ -1,20 +1,35 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  NgModule
+} from '@angular/core';
 
+import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular/lazy';
 
-import { CaseStudiesPageRoutingModule } from './case-studies-routing.module';
+import {
+  CaseStudiesPageRoutingModule
+} from './case-studies-routing.module';
 
-import { CaseStudiesPage } from './case-studies.page';
+import {
+  CaseStudiesPage
+} from './case-studies.page';
+
+import {
+  SharedModule
+} from '../../../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
-    CaseStudiesPageRoutingModule
+    CaseStudiesPageRoutingModule,
+    SharedModule
   ],
-  declarations: [CaseStudiesPage]
+  declarations: [
+    CaseStudiesPage
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class CaseStudiesPageModule {}
