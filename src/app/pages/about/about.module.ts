7ddf2da28243f2f5c39d0854fd3602be8693b8b3
Saif Kalkeri <kalkerisaif@gmail.com>
2026-09-16 02:38:07 +0530

    feat: build company about page with brand philosophy capabilities and principles

--- src/app/pages/about/about.module.ts
+++ src/app/pages/about/about.module.ts
@@ -1,20 +1,35 @@
-import { NgModule } from '@angular/core';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  NgModule
+} from '@angular/core';
+
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { IonicModule } from '@ionic/angular/lazy';
+import {
+  AboutPageRoutingModule
+} from './about-routing.module';
 
-import { AboutPageRoutingModule } from './about-routing.module';
+import {
+  AboutPage
+} from './about.page';
 
-import { AboutPage } from './about.page';
+import {
+  SharedModule
+} from '../../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    IonicModule,
-    AboutPageRoutingModule
+    AboutPageRoutingModule,
+    SharedModule
   ],
-  declarations: [AboutPage]
+  declarations: [
+    AboutPage
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AboutPageModule {}
